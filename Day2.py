9dac3cb65174e8bfc0104adcd6bd5727b31b2b49
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2024-12-06 02:18:23 +0000

    Day 2 AoC p2

--- Day2.py
+++ Day2.py
@@ -10,57 +10,36 @@ def isConsistent(line):
 
     return False
 
-def safeDifference(line):
-    return all(0 < abs(line[i] - line[i+1]) <= 3 for i in range(len(line) -1))
+# Issue, abs() doesn't work properly for when applied with problem dampener.
+# 
+# def safeDifference(line):
+#     return all(0 < abs(line[i] - line[i+1]) <= 3 for i in range(len(line)-1))
 
+def reeseSafeDifference(levels):
+    diffs = [x - y for x, y in zip(levels, levels[1:])]
+    return all(1 <= x <= 3 for x in diffs) or all(-1 >= x >= -3 for x in diffs)
+
+def problemDampener(line):
+        possibleArrays = []
+        for i in range(len(line)):
+             possibleArrays.append(line[:i]+line[i+1:])
         
+        for array in possibleArrays:
+             if isConsistent(array) and reeseSafeDifference(array):
+                  return True
+        return False
+
 safeCount = 0
 levelList = []
+pdCheck = False
 
 for line in fileinput.input(["Day2_input.txt"]):
     levelList = list(map(int, line.split()))
     
-    if not isConsistent(levelList):
-        continue
-    
-    if not safeDifference(levelList):
-        continue
-    
-    safeCount += 1
+    if not reeseSafeDifference(levelList):
+        if problemDampener(levelList):
+            safeCount += 1
+    else:
+         safeCount += 1
 
 print(safeCount)
-        
-#       ----- Part 2: Problem Dampener -----
-
-def isConsistentPD(line):
-    consistent = True
-    for i in range(0,len(line) -1)):
-        if line[i] <= line[i+1]:
-            consistent = False
-
-    if all(line[i] > line[i+1] for i in range(0,len(line) -1)):
-        return True
-
-    return False
-
-def safeDifferencePD(line):
-    return all(0 < abs(line[i] - line[i+1]) <= 3 for i in range(len(line) -1))
-        
-
-isProblem = 0
-for line in fileinput.input(["Day2_input.txt"]):
-    levelList = list(map(int, line.split()))
-    
-    if not isConsistent(levelList):
-        isProblem += 1
-    
-    if not safeDifference(levelList):
-        isProblem += 1
-
-    if isProblem > 1:
-        continue
-    
-    if 0 < is isProblem:
-        problemDampener(line)
-    
-    safeCount += 1
